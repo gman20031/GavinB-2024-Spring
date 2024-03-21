b62e5fd8b0e2ba02f2bc611d7c2b99374140da70
Gavin B <gavin.brandenburger1@gmail.com>
2024-03-20 20:33:58 -0700

    Comit mod 7 work

---
+++ InsertionSort.cpp
@@ -0,0 +1,18 @@
+
+void InsertionSort(int* sortedArray, int arrayLength)
+{
+	for (int* it = sortedArray + 1; it < sortedArray + arrayLength; ++it)
+	{
+		// compare current integer selected to all earlier integers
+		for (int* subIt = it; subIt > sortedArray; --subIt)
+		{	
+			// If this is smaller than the interger before, swap it.
+			if (*(subIt) < *(subIt - 1))
+			{
+				int temp = *subIt;
+				*(subIt) = *(subIt - 1);
+				*(subIt - 1) = temp;
+			}
+		}
+	}
+}
